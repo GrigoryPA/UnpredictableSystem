7979f1bdc87495b0b318944fe7029b9ea040064b
Grigory <grial00.06@gmail.com>
2021-06-11 20:09:13 +0300

    Finish. It were easily.

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -4,9 +4,11 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.lang.instrument.Instrumentation;
 
 public class Display {
     public Color[][] display;
+    public Color[][] new_display;
     public int widthImage;
     public int heightImage;
     public JFrame frame;
@@ -26,35 +28,51 @@ public class Display {
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Failed to open image !");
         }
-        widthImage=image.getWidth();
-        heightImage=image.getHeight();
+        widthImage=image.getWidth()/2;
+        heightImage=image.getHeight()/2;
         display=new Color[widthImage][heightImage];
+        new_display=new Color[widthImage][heightImage];
         for (int i = 0; i < widthImage; i++)
-            for (int j = 0; j < heightImage; j++)
-                display[i][j] = new Color(image.getRGB(i,j));
-
+            for (int j = 0; j < heightImage; j++) {
+                new_display[i][j] = BigPixel(i, j);
+            }
         return;
     }
 
-    public void AddPointOnDisplay(int x, int y, Color color) {
-        display[x][y] = color;
+    public Color BigPixel(int i, int j){
+        int I=i*2, J=j*2;
+        Color p1 = new Color(image.getRGB(I,J));
+        Color p2 = new Color(image.getRGB(I+1,J));
+        Color p3 = new Color(image.getRGB(I,J+1));
+        Color p4 = new Color(image.getRGB(I+1,J+1));
+        if((p1.getRed()+p2.getRed()+p3.getRed()+p4.getRed())/4 < 128)
+            return Color.BLACK;
+        else
+            return Color.WHITE;
     }
 
     public void CreateAndOpenImage() {
         for (int x = 0; x < image.getWidth(); x++)
             for (int y = 0; y < image.getHeight(); y++) {
-                image.setRGB(x, y, display[x][y].getRGB());
+                image.setRGB(x, y, new_display[x/2][y/2].getRGB());
             }
         CreateFrameForImage(image);
+        for (int i = 0; i < widthImage; i++)
+            for (int j = 0; j < heightImage; j++) {
+                display[i][j] = new_display[i][j];
+            }
     }
 
     public void UpdateImage() {
         for (int x = 0; x < image.getWidth(); x++)
             for (int y = 0; y < image.getHeight(); y++) {
-                image.setRGB(x, y, display[x][y].getRGB());
+                image.setRGB(x, y, new_display[x/2][y/2].getRGB());
             }
-        //imageicon.setImage(image);
         label.updateUI();
+        for (int i = 0; i < widthImage; i++)
+            for (int j = 0; j < heightImage; j++) {
+                display[i][j] = new_display[i][j];
+            }
     }
 
     public void CreateFrameForImage(BufferedImage image) {
--- src/main/java/Life.java
+++ src/main/java/Life.java
@@ -1,57 +1,74 @@
 import java.awt.*;
 
-public class Life {
-    public Display D;
+public class Life{
     public static Color IsLive = Color.WHITE;
     public static Color IsDead = Color.BLACK;
+    public static int N_GEN=100;
+    public static Display D;
 
-    public Life(Display display){
-        D=display;
-    }
 
-    public void Iteration(){
-        for(int i=0;i<D.widthImage;i++) {
+    public static void Iteration(){
+        for(int i = 0; i < D.widthImage; i++) {
             for (int j = 0; j < D.heightImage; j++) {
-                if(D.image.getRGB(i,j) ==IsDead.getRGB() && CountLife(i,j)==3)
-                    D.display[i][j]=IsLive;
-                if(D.image.getRGB(i,j) ==IsLive.getRGB() && CountLife(i,j)!=3)
-                    D.display[i][j]=IsDead;
+                D.new_display[i][j]=IsDead;
+                if(D.display[i][j] == IsDead && CountLife(i,j)==3)
+                    D.new_display[i][j]=IsLive;
+                if(D.display[i][j] == IsLive){
+                    if(CountLife(i,j)==3||CountLife(i,j)==2)
+                        D.new_display[i][j]=IsLive;
+                    else
+                        D.new_display[i][j]=IsDead;
+                }
             }
         }
     }
 
-    public int CountLife(int i, int j){
+    public static int CountLife(int i, int j){
         int count=0;
 
         if(j+1<D.heightImage){
-            if(D.image.getRGB(i,j+1)==IsLive.getRGB())
+            if(D.display[i][j+1]==IsLive)
                 count++;
             if(i+1<D.widthImage)
-                if(D.image.getRGB(i+1,j+1)==IsLive.getRGB())
+                if(D.display[i+1][j+1]==IsLive)
                     count++;
             if(i-1>=0)
-                if(D.image.getRGB(i-1,j+1)==IsLive.getRGB())
+                if(D.display[i-1][j+1]==IsLive)
                     count++;
         }
         if(j-1>=0){
-            if(D.image.getRGB(i,j-1)==IsLive.getRGB())
+            if(D.display[i][j-1]==IsLive)
                 count++;
             if(i+1<D.widthImage)
-                if(D.image.getRGB(i+1,j-1)==IsLive.getRGB())
+                if(D.display[i+1][j-1]==IsLive)
                     count++;
             if(i-1>=0)
-                if(D.image.getRGB(i-1,j-1)==IsLive.getRGB())
+                if(D.display[i-1][j-1]==IsLive)
                     count++;
         }
         if(i+1<D.widthImage){
-            if(D.image.getRGB(i+1,j)==IsLive.getRGB())
+            if(D.display[i+1][j]==IsLive)
                 count++;
         }
         if(i-1>=0){
-            if(D.image.getRGB(i-1,j)==IsLive.getRGB())
+            if(D.display[i-1][j]==IsLive)
                 count++;
         }
 
         return count;
+    }
+
+    public static void main(String[] args) {
+        D = new Display();
+        D.CreateAndOpenImage();
+        for(int i=0;i<N_GEN;i++){
+            try {
+                Thread.sleep(500);
+                Iteration();
+                D.UpdateImage();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
